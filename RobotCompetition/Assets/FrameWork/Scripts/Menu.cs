9656ee0b1c2c5abd466ebd550ac5f36640dab93b
Willem Meijer <willem@8d-games.nl>
2018-09-30 15:19:16 +0200

    Updated menuscreen

---
+++ RobotCompetition/Assets/FrameWork/Scripts/Menu.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+	private static Menu instance;
+
+	public enum Scene { MenuScene, TournamentScene, VersusScene, DungeonScene};
+
+
+	public static Menu Singleton()
+	{
+		return instance;
+	}
+
+
+	private void Awake()
+	{
+		if (instance != null && instance != this)
+			Destroy(instance.gameObject);
+
+		instance = this;
+
+		DontDestroyOnLoad(gameObject);
+	}
+
+
+	public static void SwitchScene(Scene scene)
+	{
+		SceneManager.LoadScene(scene.ToString());
+	}
+
+
+	public void SwitchToMenu()
+	{
+		SwitchScene(Scene.MenuScene);
+	}
+
+	public void SwitchToTournament()
+	{
+		SwitchScene(Scene.TournamentScene);
+	}
+
+	public void SwitchToDungeon()
+	{
+		SwitchScene(Scene.DungeonScene);
+	}
+
+	public void SwitchToVersus()
+	{
+		SwitchScene(Scene.VersusScene);
+	}
+
+}
