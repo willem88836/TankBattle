dfa3f1c664c1910a3849bc8278daa96cc9feedb1
Justin Geschiere <j.geschiere@8d-games.nl>
2018-10-11 09:14:43 +0200

    Ja prima

---
+++ RobotCompetition/Assets/Resources/AIscripts/TomKattouw.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TomKattouw : RobotControl {
+
+	//Only change color to white
+	//Mainly for testing purposes of your AI against this AI
+
+	// Cannot call Awake
+
+	TankData _ownTankData;
+	TankData[] _enemyData;
+
+	float _movePower = 1;
+	float _tankRotation = 0;
+	Vector2 _tankRotationMinMax = new Vector2(-30, 30);
+	float _sensorDirection = 90;
+	bool _tankFound = false;
+
+	float _changeMoveTimer = 3;
+	Vector2 _changeMoveMinMax = new Vector2(1, 4);
+
+	void Start()
+	{
+		// Called once when the tank is spawned
+	}
+
+	void Update()
+	{
+		_ownTankData = GetOwnData();
+		TankData[] _enemyData = ReadSensor();
+
+		if (_enemyData.Length > 0)
+			_tankFound = true;
+
+		if (_enemyData.Length == 0 && _tankFound == true)
+		{
+			_sensorDirection *= -1;
+			_tankFound = false;
+		}
+
+		Movement();
+		RotateSensor();
+		Aim();
+		Shoot();
+	}
+
+	void Aim()
+	{
+		SetGunAngle(_ownTankData.SensorAngle);
+	}
+
+	void RotateSensor()
+	{
+		SetSensorAngle(_ownTankData.SensorAngle + _sensorDirection);
+	}
+
+	void Movement()
+	{
+		SetMovePower(_movePower);
+
+		_changeMoveTimer -= Time.deltaTime;
+		if (_changeMoveTimer <= 0)
+		{
+			_changeMoveTimer = Random.Range(_changeMoveMinMax.x, _changeMoveMinMax.y);
+			_movePower *= -1;
+
+			_tankRotation = Random.Range(_tankRotationMinMax.x, _tankRotationMinMax.y);
+		}
+
+		SetTankAngle(_ownTankData.SensorAngle + 90 + _tankRotation);
+	}
+
+	protected override void OnWallCollision()
+	{
+		_changeMoveTimer = 4;
+		_movePower *= -1;
+	}
+
+	protected override void OnTankCollision()
+	{
+		// Called when this tank collides with another tank
+	}
+
+	protected override void OnBulletHit()
+	{
+		// Called when this tank is hit by a bullet
+	}
+}
---
+++ RobotCompetition/Assets/Resources/AIscripts/WillemMeijer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WillemMeijer : RobotControl
+{
+	private float movePower = 1;
+
+
+	private void Start()
+	{
+		SetBodyColor(Color.blue);
+		SetTurretColor(Color.blue);
+	}
+
+	private void Update()
+	{
+		TankData ownData = GetOwnData();
+		List<TankData> sensorData = new List<TankData>(ReadSensor());
+
+		SetSensorAngle(ownData.SensorAngle + 1);
+		SetGunAngle(ownData.SensorAngle + 1);
+
+		if (sensorData.Count == 0)
+		{
+			Debug.Log(" nope");
+		}
+		else
+		{
+			Debug.Log(" FOUND AND AENENEMT");
+			float minDistance = float.MaxValue;
+			TankData target = null;
+			
+			foreach (TankData tank in sensorData)
+			{
+				float distance = (tank.Position - ownData.Position).sqrMagnitude;
+				if (distance < minDistance)
+				{
+					minDistance = distance;
+					target = tank;
+				}
+			}
+
+			Vector2 normal = target.Position - ownData.Position;
+			float targetAngle = Vector3.Angle(target.Position, ownData.Position);
+
+			//SetGunAngle(targetAngle);
+			//SetSensorAngle(targetAngle); // TODO: Change this to somehting that makes sense. 
+			Shoot();
+		}
+		SetTankAngle(ownData.TankAngle + Mathf.Sin(Time.deltaTime) * 20);
+		SetMovePower(movePower);
+		
+	}
+
+	protected override void OnWallCollision()
+	{
+		Debug.Log(" colliding!");
+		movePower *= -1;
+	}
+	protected override void OnTankCollision()
+	{
+		Debug.Log(" shooting");
+		Shoot();
+		movePower *= -1;
+	}
+}
