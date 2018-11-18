4c2b22b5e3c184b1da7d7f037c97dd2e6064a27b
Justin Geschiere <j.geschiere@8d-games.nl>
2018-11-18 17:03:14 +0100

    Updated and added old example AI's

---
+++ RobotCompetition/Assets/Resources/AIscripts/ExampleAI1.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleAI1 : TankController {
+
+    //Variable that holds the current movePower
+    private int movePower = 1;
+
+	void Start ()
+    {
+		//Changes the robot color
+		SetBodyColor(Color.blue);
+		SetTurretColor(Color.blue);
+	}
+	
+    //Called every frame
+	void Update () {
+		//Sets the movement movepower to the current movePower variable
+		SetMovePower(movePower);
+
+		//Make the robot rotate towards a 90 degree angle
+		SetTankAngle(90);
+
+		//Make the robot's sensor constantly rotating;
+		SetSensorAngle(-90 + GetOwnData().SensorAngle);
+
+        //Make the robot's gun constantly follow the sensor (giving the same angle values)
+        SetGunAngle(-90 + GetOwnData().SensorAngle);
+
+        //If a robot is within the sensor, start shooting
+        if (ReadSensor().Length > 0)
+        {
+            //Shoot bullet
+            Shoot();
+        }
+	}
+
+    protected override void OnWallCollision()
+    {
+        //move in the opposite direction when colliding with a wall
+        movePower *= -1;
+    }
+
+    protected override void OnTankCollision()
+    {
+        //move in the opposite direction when colliding with another robot
+        movePower *= -1;
+    }
+}
---
+++ RobotCompetition/Assets/Resources/AIscripts/ExampleAI3.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+
+public class ExampleAI3 : TankController {
+
+    // This tank will periodically move forwards or backwards, switching every 4 seconds
+    // The scanner will turn clockwise, along with the turret. As long as another tank is in the scanner, it will keep shooting
+    // when a tank leaves the scanner area, the scanner will rotate in the oposite direction
+
+    private float moveTimer= 4f;    // The timer used to move forward and backwards. Also used to set the speed for the tank
+    private bool moveForward;       // Checks if the tank is moving forward or backward
+
+    private int scanDirection = 90; // This is used to rotate the scanner in a direction. Will be set to negative to turn it counterclockwise
+
+    private int otherTanksStored = 0; // the amount of other tanks found. This is used to check if a tank leaves the scanner, to make the scanner turn in another direction 
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Movement(); // controls the movement
+        Scan(); // controls the scanner and shooting
+	}
+
+    void Movement()
+    {
+        // move forward and backward. Switch every 4 seconds
+        moveTimer -= Time.deltaTime;
+        if (moveTimer <= 0)
+        {
+            moveTimer = 4;
+            moveForward = !moveForward;
+        }
+
+        // the movePower is equal to the moveTimer. Whenever this is > 1, the script will automatically reduce the movePower to the maximum of 1 (or -1)
+        // this also means that the movespeed will be reduced whenver the movePower will reduce when moveTimer goes below 1 
+        if (moveForward) 
+			SetMovePower(moveTimer);
+        else
+            SetMovePower(-moveTimer);
+    }
+
+    void Scan()
+    {
+        // scan in circles. If a tank leaves the scanner, turn the scanner in the other direction
+        TankData[] otherTanks = ReadSensor();
+
+        // if a tank is seen, shoot
+        if (otherTanks.Length > 0)
+        {
+            Shoot();
+        }
+
+        // this part checks if a tank has left the scanner area. If thats true, it will scan in the oposite direction
+        if (otherTanksStored > otherTanks.Length)
+        {
+            scanDirection = -scanDirection;
+        }
+        otherTanksStored = otherTanks.Length;
+
+        SetSensorAngle(scanDirection + GetOwnData().SensorAngle);
+        SetGunAngle(scanDirection + GetOwnData().SensorAngle);
+    }
+}
