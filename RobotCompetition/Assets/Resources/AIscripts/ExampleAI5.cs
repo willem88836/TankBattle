0d6b0c2f8c4e637834af0ee611f8fe68b0786ddd
TomKattouw <tomkattouw@hotmail.com>
2018-11-20 17:48:35 +0100

    Testing for VS error

---
+++ RobotCompetition/Assets/Resources/AIscripts/ExampleAI5.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleAI5 : TankController
+{
+	
+
+	void Start ()
+	{
+		//Change the color of the tank
+		SetBodyColor(Color.red);
+		SetTurretColor(Color.white);
+	}
+
+	void Update ()
+	{
+		
+	}
+}
